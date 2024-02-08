149559c9731d78267ad72a199750d7629a8fa62b
Aditya prasad <71696618+yupitsadi@users.noreply.github.com>
2024-02-08 14:26:12 +0530

    Updated Solution - GfG to GitHub

--- Easy/Rotate Array/rotate-array.cpp
+++ Easy/Rotate Array/rotate-array.cpp
@@ -9,21 +9,10 @@ class Solution{
     
     //Function to rotate an array by d elements in counter-clockwise direction. 
     void rotateArr(int arr[], int d, int n){
-        if (n == 0)
-        return;
-        d = d % n;
-        if (d > n)
-        return;
-        int temp[d];
-        for(int i=0; i<d; i++){
-            temp[i]=arr[i];
-        }
-        for(int i=0; i<n-d; i++){
-            arr[i]=arr[i+d];
-        }
-        for(int i=n-d; i<n; i++){
-            arr[i]=temp[i-n+d];
-        }
+        int c=d%n;
+        reverse(arr,arr+c);
+        reverse(arr+c,arr+n);
+        reverse(arr,arr+n);
     }
 };
 
